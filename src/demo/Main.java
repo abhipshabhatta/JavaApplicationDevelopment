8ddeaa0fbcadde656247c88559aa1c6f4acba2db
Abhipsha Bhatta <abhipshabhatta@Abhipshas-MacBook-Pro.local>
2023-10-30 23:20:09 +0100

    second commit

---
+++ src/demo/Main.java
@@ -0,0 +1,36 @@
+package demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+	/*datatype.datatype();
+		
+	String value = "StringValues";
+	System.out.println("String: "+ value);*/
+		
+	ArrayExample arr = new ArrayExample();
+	arr.arrayEx();
+		
+	/*StringExample = new StringExample();
+	examp.stringExercise();
+		
+	ConditionalStatement = new ConditionalStatement();
+	stms.voteCheck();
+		
+	/*LoopExercise loop = new LoopExercise();
+	loop.reverse();*/
+	
+	/* x = 5, y = 10;
+	System.out.println("10" + "3" + "Hello"); 
+	System.out.println(10 + 3 + "Hello");
+	System.out.println("10" + 3.4 + 5 +"Hello");
+	System.out.println("10" + "3" + "Hello" + x + y);
+	System.out.println(x + y + "Hello");*/
+
+
+
+
+		
+	}
+
+}
